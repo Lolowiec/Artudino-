46562b45ad4217fcd505b6f882631d16c07fefd6
Lolowiec <138101574+Lolowiec@users.noreply.github.com>
2024-09-08 15:43:25 +0200

    Add files via upload

--- 12312312.ino
+++ 12312312.ino
@@ -1,20 +1,21 @@
-int licznik = 1;
+int counter = 0;
 
 void setup() {
   Serial.begin(115200);
-
-  while (!Serial) {
-    ;
-  }
-
-  Serial.println("Hello World 1");
 }
 
 void loop() {
+  std::String message_template = "Hello World";
+
+  for (int i = 0; i < message_template.length(); i++) {
+    Serial.print(message_template[i]); 
+    if (i < message_template.length() - 1) {
+      Serial.print(",");  
+    }
+  }
+  
+  Serial.println();
+
+  counter += 1;
   delay(1000);
-
-  licznik++;
-
-  Serial.print("Hello World ");
-  Serial.println(licznik);
 }
